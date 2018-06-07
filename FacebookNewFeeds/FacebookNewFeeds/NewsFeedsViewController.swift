e6135a4d9558ccb6e1407cf3f93bb2b24b0ead8c
Ha Thi Hoan <>
2018-06-07 16:37:26 +0700

    [Hoan][UI] Improve UI.

--- FacebookNewFeeds/FacebookNewFeeds/NewsFeedsViewController.swift
+++ FacebookNewFeeds/FacebookNewFeeds/NewsFeedsViewController.swift
@@ -13,9 +13,13 @@ private struct Constants {
     public static let friendCollectionViewCellIdentifier = "FriendCollectionViewCell"
     public static let newsFeedTableViewCellNibName = "NewsFeedTableViewCell"
     public static let newsFeedTableViewCellIdentifier = "NewsFeedTableViewCell"
+    public static let loadMoreTableViewCellNibName = "LoadMoreTableViewCell"
+    public static let loadMoreTableViewCellIdentifier = "LoadMoreTableViewCell"
+    public static let heightLoadMoreCell: CGFloat = 44
     public static let url = "https://www.mocky.io/v2/5b0ce0cb3300005100b400f1"
     public static let profileViewControllerIdentifier = "ProfileViewController"
     public static let colorSearchBarBackground = "#2E4780"
+    public static let numberNewsFeedShow = 10
 }
 
 class NewsFeedsViewController: UIViewController {
@@ -24,17 +28,20 @@ class NewsFeedsViewController: UIViewController {
     @IBOutlet weak var newsFeedTableView: UITableView!
     var feedsArray: [FeedModel] = []
     var storyArray: [StoryModel] = []
+    var feedsTempArray: [FeedModel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNavigationBarItems()
         friendsCollectionView.dataSource = self
-        friendsCollectionView.register(UINib.init(nibName: Constants.friendCollectionViewCellNibName, bundle: nil),
+        friendsCollectionView.register(UINib(nibName: Constants.friendCollectionViewCellNibName, bundle: nil),
                                        forCellWithReuseIdentifier: Constants.friendCollectionViewCellIdentifier)
         newsFeedTableView.dataSource = self
         newsFeedTableView.delegate = self
-        newsFeedTableView.register(UINib.init(nibName: Constants.newsFeedTableViewCellNibName, bundle: nil),
+        newsFeedTableView.register(UINib(nibName: Constants.newsFeedTableViewCellNibName, bundle: nil),
                                    forCellReuseIdentifier: Constants.newsFeedTableViewCellIdentifier)
+        newsFeedTableView.register(UINib(nibName: Constants.loadMoreTableViewCellNibName, bundle: nil),
+                                   forHeaderFooterViewReuseIdentifier: Constants.loadMoreTableViewCellIdentifier)
         QueryService.get(context: self, url: Constants.url) { (response) in
             guard let responseFeedData = response["feeds"] as? [[String: Any]] else {
                 return
@@ -42,6 +49,9 @@ class NewsFeedsViewController: UIViewController {
             for feed in responseFeedData {
                 let feedModel = FeedModel.init(response: feed)
                 self.feedsArray.append(feedModel)
+                if self.feedsTempArray.count < Constants.numberNewsFeedShow {
+                    self.feedsTempArray.append(feedModel)
+                }
             }
             DispatchQueue.main.async {
                 self.newsFeedTableView.reloadData()
@@ -97,17 +107,47 @@ extension NewsFeedsViewController: UICollectionViewDataSource {
 extension NewsFeedsViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return feedsArray.count
+       return feedsTempArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.newsFeedTableViewCellIdentifier,
-                                                       for: indexPath) as? NewsFeedTableViewCell else {
-        return UITableViewCell()
+            for: indexPath) as? NewsFeedTableViewCell else {
+                return UITableViewCell()
         }
-        let feedModel = feedsArray[indexPath.row]
+        let feedModel = feedsTempArray[indexPath.row]
         cell.setContent(feedModel: feedModel)
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: Constants.loadMoreTableViewCellIdentifier)
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return feedsArray.count == feedsTempArray.count ? 0.0001 : Constants.heightLoadMoreCell
+    }
+
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastElement = feedsTempArray.count - 1
+        if indexPath.row == lastElement {
+            let beginElement = feedsTempArray.count
+            var endElement = 0
+            if feedsArray.count > (feedsTempArray.count + Constants.numberNewsFeedShow) {
+                endElement = feedsTempArray.count + Constants.numberNewsFeedShow - 1
+            } else {
+                endElement = feedsArray.count - 1
+            }
+            if beginElement <= endElement {
+                for index in beginElement...endElement {
+                    feedsTempArray.append(feedsArray[index])
+                }
+                DispatchQueue.main.async {
+                    self.newsFeedTableView.reloadData()
+                }
+            }
+        }
     }
 
 }
@@ -117,9 +157,10 @@ extension NewsFeedsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
         guard let profileViewController = storyboard?.instantiateViewController(
-            withIdentifier: Constants.profileViewControllerIdentifier) else {
+            withIdentifier: Constants.profileViewControllerIdentifier) as? ProfileViewController else {
                 return
         }
         navigationController?.pushViewController(profileViewController, animated: true)
     }
+
 }
